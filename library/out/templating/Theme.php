d9bb7462714421a131b790441b4556a925b8839e
Boris Wintein <boris.wintein@telenet.be>
2013-09-23 07:39:09 +0200

    Wrapper Class, still figuring out templating algorithm

--- library/out/templating/Theme.php
+++ library/out/templating/Theme.php
@@ -10,9 +10,15 @@ namespace Molly\library\out\templating;
 
 use \Molly\library\io\dataloaders\files\FileLoader as FileLoader;
 use \Molly\library\io\dataloaders\files\File as File;
+
 use \Molly\library\exceptions\IllegalArgumentException as IllegalArgumentException;
 
- class Theme {
+use \Molly\library\utils\collection\MollyArray as MollyArray;
+
+use \Molly\library\utils\xml\node\XMLNode as XMLNode;
+use \Molly\library\utils\xml\XMLFile as XMLFile;
+
+class Theme {
     private static $singleton;
 
     private $fileloader;
@@ -28,10 +34,10 @@ use \Molly\library\exceptions\IllegalArgumentException as IllegalArgumentExcepti
     }
 
     private function __construct() {
-
+        require_once(getcwd() . "/library/utils/html/simple_html_dom.php");
     }
 
-    public function setFile($file) {
+    public function setFile(File $file) {
         if (!$file instanceof File) {
             throw new IllegalArgumentException("Argument must be instance of File");
         }
@@ -39,48 +45,66 @@ use \Molly\library\exceptions\IllegalArgumentException as IllegalArgumentExcepti
         $this->file = $file;
     }
 
-     public function setFileloader($fileloader)
-     {
-         if (!$fileloader instanceof FileLoader) {
-             throw new IllegalArgumentException("Argument must be instance of FileLoader");
-         }
+    public function setFileloader(FileLoader $fileloader)
+    {
+        if (!$fileloader instanceof FileLoader) {
+            throw new IllegalArgumentException("Argument must be instance of FileLoader");
+        }
 
-         $this->fileloader = $fileloader;
-     }
+        $this->fileloader = $fileloader;
+    }
 
     public function render() {
         $this->fileloader->addExpectedFileLocation($this->file->getLocation());
         $this->file = $this->fileloader->load($this->file);
 
         // Create the template
-        $this->template = new \SimpleXMLElement($this->file->getContent());
+        $this->template = str_get_html($this->file->getContent());
 
-        $templatevars = array("productlist" => "testtest");
-        echo "<pre>";
-        print_r($templatevars);
-        $this->template = $this->parseElement($this->template, $templatevars);
-
-        echo $this->template->asXML();
+        $vars = array(
+            "logo" =>
+                array("attributes" => array("href" => "molly.thisisboris.be"),
+                    "website_name" => "Thisisboris.be"
+                ),
+            "navigation" =>
+                array(
+                    "navigation-item" => array(
+                        array("content" => "home", "attributes" => array("href" => "/home")),
+                        array("content" => "info", "attributes" => array("href" => "/info")),
+                        array("content" => "blog", "attributes" => array("href" => "/blog")),
+                        array("content" => "contact", "attributes" => array("href" => "/contact"))
+                    )
+                )
+        );
     }
 
-     private function parseElement($element, $elementVariable = null) {
-         $elementAttributes = $element->attributes();
-         if (isset($elementAttributes['theme'])) {
-            $themeAttribute = (string)$elementAttributes['theme'];
-            if (is_string($elementVariable[$themeAttribute])) {
-                $element[0] = $elementVariable[$themeAttribute];
-            } else if (is_array($elementVariable[$themeAttribute])) {
-                // @TODO Loop elements.
-                foreach($elementVariable[$themeAttribute] as $key => $subvariable) {
+     private function parseElement(\DOMNode $node, $rootElement = null, $elementVariable = null) {
 
-                }
-            }
-         } else {
-             foreach ($element->children() as $childElement) {
-                 $childElement = $this->parseElement($childElement, $elementVariable);
+         if ($node instanceof \DOMElement) {
+             echo "START DOMElement:" . $node->nodeName . "<br/>";
+
+             if ($node->hasAttribute("theme")) {
+                 echo "Node theme tag: " . $node->getAttribute("theme") . "<br/>";
+                 $themeAttribute = $node->getAttribute("theme");
+                 if (isset($elementVariable[$themeAttribute])) {
+                    if (is_array($elementVariable[$themeAttribute])) {
+
+                    } else if (is_string($elementVariable[$themeAttribute])) {
+
+                    }
+                 }
+
+             } else {
+                 foreach($node->childNodes as $childnode) {
+                     $this->parseElement($childnode, $node, $elementVariable);
+                 }
              }
+
+             echo "STOP DOMElement:" . $node->nodeName . "<br/>";
+         } else if ($node instanceof \DOMText && !$node->isElementContentWhitespace()) {
+             echo "parsing DOMText:" . $node->nodeName . "<br/>";
          }
 
-         return $element;
+         return $node;
      }
 }
