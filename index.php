375b2c8e3c7385127c011cc8a836f1448ca9e117
boris <boris.wintein@telenet.be>
2014-03-23 07:15:09 +0100

    'It has been a while-commit' Meet lucy the library, it needed it's own namespace. Why? Because I say so.

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * This file is part of Molly, an open-source content manager.
+ * This file is part of molly, an open-source content manager.
  *
  * This application is licensed under the Apache License, found in LICENSE.TXT
  *
- * Molly CMS - Written by Boris Wintein
+ * molly CMS - Written by Boris Wintein
  */
 // Require our libary autoloader.
-require_once("library/toolbelt/Classloader.php");
+require_once("lucy/toolbelt/Classloader.php");
 
 $start_time = microtime(TRUE);
 
-$letter = new \Molly\library\out\messages\Letter(\Molly\library\out\messages\Letter::INFORMATION);
+$letter = new \Lucy\out\messages\Letter(\Lucy\out\messages\Letter::INFORMATION);
 
 $letter->setHead('This Message');
 $letter->setContents('has contents');
